f47346c6cf5aa95a6119f722fa7955c1923a414d
fridahharawo <derasoharawo@gmail.com>
2026-02-14 17:21:08 +0300

    arrays

---
+++ array.py
@@ -0,0 +1,22 @@
+# fridah deraso
+# implementing arrays in python using list
+
+#creating an empty array
+names =[]
+
+# we add items using append()
+names.append("naserian")
+names.append("najma")
+names.append("sasha")
+names.append("ruweidha")
+names.append("shalin")
+
+
+# we remove items at the end using pop().
+# pop() returns the element that it has removed and which can be stored in a variable
+lastname= names.pop() # the last element will be abind to the lastname variable
+
+#we acces element using indexing 
+#indexes start from 0
+first_element=names[0]
+last_element=names[-1]
